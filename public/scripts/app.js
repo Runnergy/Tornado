9ea03f808c1a10cb73e3e765c48792b91de81feb
heesoolim <hlim29@my.centennialcollege.ca>
2020-11-12 19:23:49 -0500

    added comments and function determining winner

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -16,6 +16,39 @@
                 }
             });
         }
+
+        // determine winner for each round
+        determinWinner('first', 'second');
+        determinWinner('second', 'third');
+        determinWinner('third', 'fourth');
+        determinWinner('fourth', 'fifth');
+        
+    }
+
+    function determinWinner(currentRound, nextRound)
+    {
+        // current round's participants
+        let roundParticipants = document.getElementsByClassName(currentRound);
+        
+        for (let index = 0; index < roundParticipants.length; index++) 
+        {
+            // when one of the participant object is clicked
+            roundParticipants[index].addEventListener('click', (e) => {
+                // calculate the index to display clicked object on the next round's object
+                nextIndex = Math.floor((index)/2);
+
+                // next round's participants
+                let nextRoundParticipants = document.getElementsByClassName(nextRound);
+
+                // assign clicked current participant's number to the next round's winner number
+                nextRoundParticipants[nextIndex].querySelector('.participant-num').innerText = 
+                    roundParticipants[index].querySelector('.participant-num').innerText;
+
+                // assign clicked current participant's name to the next round's winner name
+                nextRoundParticipants[nextIndex].querySelector('.participant-name').innerText = 
+                    roundParticipants[index].querySelector('.participant-name').innerText;
+            });
+        }
     }
     
     window.addEventListener("load", Start);
