1e410b4f08828afc9c512b35dc7821de6c3cb0c9
heesoolim <hlim29@my.centennialcollege.ca>
2020-11-16 14:23:27 -0500

    Added a demo active tournament table

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -5,10 +5,25 @@ let mongoose = require('mongoose');
 
 //create the User Model instance\
 let userModel = require('../models/user');
+
 let User = userModel.User; // alias
 
+// create a reference to the model
+let Tournament = require('../models/tournament');
+
 module.exports.displayHomePage = (req, res, next) => {
-    res.render('index', { title: 'Home', file: './partials/home', displayName: req.user ? req.user.displayName : ''});
+    Tournament.find((err, tournamentList) => {
+        if(err)
+        {
+            return console.error(err);
+        }
+        else
+        {
+            // console.log(tournamentList);
+            res.render('index', {title: 'Home',file: './partials/home', TournamentList: tournamentList, displayName: req.user ? req.user.displayName : ''});      
+        }
+    });
+    // res.render('index', { title: 'Home', file: './partials/home', displayName: req.user ? req.user.displayName : ''});
 }
 
 module.exports.displayAboutPage = (req, res, next) => {
