b4d6e48ea0748f2783a8a9dbef74a1ab08eff04f
Gurkirat Khaira <777gurkirat@gmail.com>
2020-11-17 21:04:08 -0500

    update routes-controllers for login, register and user logout

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -34,10 +34,95 @@ module.exports.displayContactPage = (req, res, next) => {
     res.render('index', { title: 'Contact Us', file: './partials/contact', displayName: req.user ? req.user.displayName : '' });
 }
 
+module.exports.displayLoginPage = (req, res, next) => {
+    //check if the user is already logged in
+    if (!req.user) {
+        res.render('auth/login', {
+            title: "Login",
+            messages: req.flash('loginMessage'),
+            displayName: req.user ? req.user.displayName : ''
+        })
+    }
+    else {
+        return res.redirect('/');
+    }
+}
 
-/*
+module.exports.processLoginPage = (req, res, next) => {
+    passport.authenticate('local',
+        (err, user, info) => {
+            //server error
+            if (err) {
+                return next(err);
+            }
+            //is there a user login error?
+            if (!user) {
+                req.flash('loginMessage', 'Authentication Error');
+                return res.redirect('/login');
+            }
+            req.login(user, (err) => {
+                // server err
+                if (err) {
+                    return next(err);
+                }
+                return res.redirect('/contact-list');
+            })
+        })(req, res, next);
+}
+
+module.exports.displayRegisterPage = (req, res, next) => {
+    //if user is not already logged in
+    if (!req.user) {
+        res.render('auth/register',
+        {
+            title: "Register",
+            messages: req.flash('registerMessage'),
+            displayName: req.user ? req.user.displayName : ' '
+        });
+    }
+    else {
+        return res.redirect('/');
+    }
+}
+
+module.exports.processRegisterPage = (req, res, next) => {
+    // instantiate a user object
+    let newUser = new User({
+        username: req.body.username,
+        //password: req.body.password
+        email: req.body.email,
+        displayName: req.body.displayName
+    });
+
+    User.register(newUser, req.body.password, (err) => {
+        if (err) {
+            console.log("Error: Inserting New user");
+            if (err.name == "UserExistsError") {
+                req.flash(
+                    'registerMessage',
+                    'Registration Error: User Already Exists!'
+                );
+                console.log('Error, user already exists');
+            }
+            return res.render('auth/register', {
+                title: 'Register',
+                messages: req.flash('registerMessage'),
+                displayName: req.user ? req.user.displayName : ' '
+            });
+        }
+        else {
+            //if no error exists, then registration is successsful
+
+            //redirect the user and authenticate them
+
+            return passport.authenticate('local')(req, res, () => {
+                res.redirect('/contact-list')
+            });
+        }
+    });
+}
+
 module.exports.performLogout = (req, res, next) => {
     req.logout();
     res.redirect('/');
 }
-*/
--- server/routes/index.js
+++ server/routes/index.js
@@ -15,7 +15,19 @@ router.get('/about', indexController.displayAboutPage);
 /* GET contact us page. */
 router.get('/contact', indexController.displayContactPage);
 
+/** GET Route for displaying the Login page  */
+router.get('/login', indexController.displayLoginPage);
+
+/** POST Route for processing the Login page  */
+router.post('/login', indexController.processLoginPage);
+
+/** GET Route for displaying the Register page  */
+router.get('/register', indexController.displayRegisterPage);
+
+/** POST Route for processing the Register page  */
+router.post('/register', indexController.processRegisterPage);
+
 /** GET to perform UserLogout */
-//router.get('/logout', indexController.performLogout);
+router.get('/logout', indexController.performLogout);
 
 module.exports = router;
