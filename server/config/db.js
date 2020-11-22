a4b7750a226f93777784addc04c77c0af2f38477
heesoolim <hlim29@my.centennialcollege.ca>
2020-11-22 15:37:58 -0500

    changed mongodb URI

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,5 @@
 module.exports =
 {
     // "URI": "mongodb://localhost/tornado",
-    "URI": "mongodb+srv://heesoo:eUUjodz9MY6sXbKx@dbserver.pajxh.mongodb.net/tornado?retryWrites=true&w=majority",
-    // "URI": "mongodb+srv://chaeeun:FmnAYVExbJBNvLi5@dbserver.v1jag.mongodb.net/tornado?retryWrites=true&w=majority",
-    
+    "URI": "mongodb+srv://chaeeun:FmnAYVExbJBNvLi5@dbserver.v1jag.mongodb.net/tornado?retryWrites=true&w=majority",
 }
