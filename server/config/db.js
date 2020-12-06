55a9db81e0c4040687dae793d12a200ca8692088
chaeeun kim <afafpo@naver.com>
2020-12-07 00:17:58 +0900

    changed db URI

--- server/config/db.js
+++ server/config/db.js
@@ -2,6 +2,6 @@ module.exports =
 {
     // mongoDB URIs
     // "URI": "mongodb://localhost/tornado",
-    "URI": "mongodb+srv://heesoo:yJAlHvFTRklzRmkJ@dbserver.pajxh.mongodb.net/tornado?retryWrites=true&w=majority",
-    // "URI": "mongodb+srv://chaeeun:FmnAYVExbJBNvLi5@dbserver.v1jag.mongodb.net/tornado?retryWrites=true&w=majority",
+   // "URI": "mongodb+srv://heesoo:yJAlHvFTRklzRmkJ@dbserver.pajxh.mongodb.net/tornado?retryWrites=true&w=majority",
+     "URI": "mongodb+srv://chaeeun:FmnAYVExbJBNvLi5@dbserver.v1jag.mongodb.net/tornado?retryWrites=true&w=majority",
 }
